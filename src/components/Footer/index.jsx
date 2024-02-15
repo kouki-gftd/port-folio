1228806ddc5f28b4ba381b96ba9a967d60d9c3c6
kouki-gftd <k.kanamori@gftd.works>
2024-02-15 13:51:00 +0900

    Remove container from footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,7 +7,7 @@ import logoFb from "public/facebook.png";
 export const Footer = () => {
   return (
     <>
-      <div className="container bg-bg-black flex flex-col items-center justify-center">
+      <div className="bg-bg-black flex flex-col items-center justify-center">
         <p className="text-white pt-10 pb-8">CREATED BY 名前 太郎</p>
         <Image className="py-10" src={logoWhite} alt="ロゴ" width="120" height="60" />
         <div className="flex">
