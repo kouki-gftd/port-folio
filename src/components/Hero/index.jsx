b6dfe9cbc42fecd7bd57032988fb79de9a2fba11
kouki-gftd <k.kanamori@gftd.works>
2024-03-06 11:11:30 +0900

    Adjustment of hero component layouts

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -6,9 +6,9 @@ export const Hero = () => {
   return (
     <>
       <div className="bg-bg-green md:flex items-center justify-between py-5">
-        <div className="text-white text-center whitespace-nowrap basis-1/2">
-          <h1 className="text-4xl md:ml-20 text-5xl font-bold py-5">メインタイトル</h1>
-          <p className="text-3xl md:ml-20 text-4xl py-5">サブタイトル</p>
+        <div className="text-white text-center whitespace-nowrap basis-1/2 md:ml-20 md:text-left">
+          <h1 className="text-3xl md:ml-20 md:text-5xl font-bold py-5">メインタイトル</h1>
+          <p className="text-2xl opacity-50 md:ml-20 md:text-4xl font-bold py-5">サブタイトル</p>
         </div>
         <div className="flex flex-center ml-10 md:flex justify-end pr-10 basis-1/2">
           <Image src={heroImg} alt="hero" />
